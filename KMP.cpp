be15b11a017231dfe0a6cf4af2b5708b07295585
silver9450 <rain.jirou@gmail.com>
2016-10-14 00:03:55 +0800

    Update KMP.cpp

--- KMP.cpp
+++ KMP.cpp
@@ -5,22 +5,22 @@ using namespace std;
 
 string s, j;
 vector<int> match_point;
-vector<int> j_fail;
+vector<int> fail;
 
-void half_KMP(string& s, string& j, vector<int>& j_fail)
+void half_KMP(string& s, string& j, vector<int>& fail)
 {
-    if((&s) == (&j)) j_fail.push_back(-1);
+    bool build_fail = ((&s) == (&j));
+    if(build_fail) fail.push_back(-1);
     for(int i = 0, fail_bit = -1; i < (int)s.size(); ++i)
     {
-        if(i == 0 && (&s) == (&j)) continue;
-        while(s[i] != j[fail_bit + 1] && (fail_bit >= 0)) fail_bit = j_fail[fail_bit];
+        if(i == 0 && build_fail) continue;
+        while(s[i] != j[fail_bit + 1] && (fail_bit >= 0)) fail_bit = fail[fail_bit];
         if(s[i] == j[fail_bit + 1]) fail_bit++;
-        else fail_bit = -1;
-        if((&s) == (&j)) j_fail.push_back(fail_bit);
-        if((&s) != (&j) && fail_bit == (int)(j.size() - 1))
+        if(build_fail) fail.push_back(fail_bit);
+        if(!build_fail && fail_bit == (int)(j.size() - 1))
         {
             match_point.push_back(i - j.size() + 1);
-            fail_bit = j_fail[fail_bit];
+            fail_bit = fail[fail_bit];
         }
     }
 }
@@ -31,15 +31,10 @@ int main()
     cin >> s;
     cout << "Please input J: ";
     cin >> j;
-    half_KMP(j, j, j_fail);
-    half_KMP(s, j, j_fail);
-/*
-    cout << "Fail function of S: ";
-    for(int i = 0; i < (int)s.size(); ++i) cout << s_fail[i] << ' ';
-    cout << endl;
-*/
+    half_KMP(j, j, fail);
+    half_KMP(s, j, fail);
     cout << "Fail function of J: ";
-    for(int i = 0; i < (int)j.size(); ++i) cout << j_fail[i] << ' ';
+    for(int i = 0; i < (int)j.size(); ++i) cout << fail[i] << ' ';
     cout << endl;
     cout << "Match point(s): ";
     for(auto i: match_point) cout << i << ' ';
